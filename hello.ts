22a4eb6982631ee1970906716a4821bc0e5541fe
Abhishek <abhi.ghosh27feb@gmail.com>
2022-01-12 17:34:11 +0530

    first commit

---
+++ hello.ts
@@ -0,0 +1,15 @@
+console.log("hello")
+//declaration
+// 
+var a;
+var b:number;
+var c:number=0;
+b=Number(prompt("enter a no"))
+// oprators
+// arthemetic operator
+
+
+// + - / * **
+
+
+console.log("square",b**2 )
---
+++ scripts/hello.js
@@ -0,0 +1,14 @@
+"use strict";
+console.log("hello");
+//declaration
+// 
+var a;
+var b;
+var c = 0;
+b = Number(prompt("enter a no"));
+// oprators
+// arthemetic operator
+// + - / * **
+console.log("square", b ** 2);
+//# sourceMappingURL=hello.js.map
+//+ - /* cal
